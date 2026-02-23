fac7234ca409c012cf9ae36d781e01a61acf0ba4
santiago lopez <santiagolopezramirez0721@gmail.com>
2026-02-22 23:38:22 -0500

    ✨ feat: implement student service with business validations

--- internal/student/service.go
+++ internal/student/service.go
@@ -1,1 +1,44 @@
 package student
+
+import "errors"
+
+type Service interface {
+	Create(name, lastName string, age int32) (*Student, error)
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{
+		repo: repo,
+	}
+}
+
+func (s *service) Create(name, lastName string, age int32) (*Student, error) {
+
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+
+	if lastName == "" {
+		return nil, errors.New("last_name is required")
+	}
+
+	if age <= 0 {
+		return nil, errors.New("age must be greater than zero")
+	}
+
+	student := &Student{
+		Name:     name,
+		LastName: lastName,
+		Age:      age,
+	}
+
+	if err := s.repo.Create(student); err != nil {
+		return nil, err
+	}
+
+	return student, nil
+}
