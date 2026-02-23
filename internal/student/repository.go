f3075fbe1c9514b40f46d49d6fb9437d263541fa
santiago lopez <santiagolopezramirez0721@gmail.com>
2026-02-22 23:29:22 -0500

    ✨ feat: implement student repository with gorm

--- internal/student/repository.go
+++ internal/student/repository.go
@@ -1,1 +1,27 @@
 package student
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type (
+	Repository interface {
+		Create(student *Student) error
+	}
+
+	repository struct {
+		db *gorm.DB
+	}
+)
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repository{
+		db: db,
+	}
+
+}
+func (r *repository) Create(student *Student) error {
+	student.ID = uuid.New().String()
+	return r.db.Create(student).Error
+}
