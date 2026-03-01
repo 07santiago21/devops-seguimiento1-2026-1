a9e6977a4eb70c404369ee7417409b3831643dfb
santiago lopez <santiagolopezramirez0721@gmail.com>
2026-03-01 12:23:14 -0500

    ✨ feat(student): add Get and GetAll methods to repository

--- internal/student/repository.go
+++ internal/student/repository.go
@@ -8,6 +8,8 @@ import (
 type (
 	Repository interface {
 		Create(student *Student) error
+		GetAll() ([]Student, error)
+		Get(id string) (*Student, error)
 	}
 
 	repository struct {
@@ -24,4 +26,28 @@ func NewRepository(db *gorm.DB) Repository {
 func (r *repository) Create(student *Student) error {
 	student.ID = uuid.New().String()
 	return r.db.Create(student).Error
+}
+
+func (r *repository) GetAll() ([]Student, error) {
+	var students []Student
+	result := r.db.Model(&students).Order("created_at desc").Find(&students)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return students, nil
+}
+
+func (r *repository) Get(id string) (*Student, error) {
+
+	student := Student{ID: id}
+	result := r.db.First(&student)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &student, nil
+
 }
